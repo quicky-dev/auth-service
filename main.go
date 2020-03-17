4e2c5e70fa7a1d574bcaae8cd2510f0b4a838b01
C3NZ <cenz@cenz.io>
2020-03-17 13:10:59 -0700

    [add] the auth router to the main package, damn my go skills are rusty

--- main.go
+++ main.go
@@ -4,15 +4,18 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/quicky-dev/auth-service/routers"
 )
 
 // Main entry point for the auth service.
 func main() {
-	e := echo.New()
+	app := echo.New()
 
-	e.GET("/", func(c echo.Context) error {
+	app.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
-	e.Logger.Fatal(e.Start(":8081"))
+	routers.AttachAuth(app)
+
+	app.Logger.Fatal(app.Start(":8081"))
 }
