b15c00df292b057b94e331245959b3dc3071de9b
C3NZ <cenz@cenz.io>
2020-03-17 22:06:26 -0700

    [fix] the naming of some files.

--- main.go
+++ main.go
@@ -3,21 +3,22 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo"
 	"github.com/quicky-dev/auth-service/routers"
 )
 
-// Main entry point for the auth service.
 func main() {
 	log.SetPrefix("[auth] ")
+
 	app := echo.New()
 
 	app.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
-	routers.AttachAuth(app)
+	routers.RegisterAuthRoutes(app)
 
-	app.Logger.Fatal(app.Start(":8081"))
+	app.Logger.Fatal(app.Start(os.Getenv("PORT")))
 }
