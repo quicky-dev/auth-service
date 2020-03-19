301ded0de7aeab5f6ab95360d2bf6ea689774f36
C3NZ <cenz@cenz.io>
2020-03-18 17:30:42 -0700

    [add] the rest of the register functionality to create links that can be used for account verification.

--- controllers/auth.go
+++ controllers/auth.go
@@ -1,9 +1,12 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo"
+	"github.com/quicky-dev/auth-service/db"
 	"github.com/quicky-dev/auth-service/util"
+	"log"
 	"net/http"
 	"os"
 )
@@ -15,7 +18,7 @@ type AuthCredentials struct {
 	Password string `json:"password"`
 }
 
-type RegisterCredentials struct {
+type RegisterRequest struct {
 	Username string `json:"username"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
@@ -31,21 +34,60 @@ type AuthError struct {
 	ErrorMsg string `json:"errorMessage"`
 }
 
+func registerRequestToUnverifiedUser(request *RegisterRequest) (*db.UnverifiedUser, error) {
+	user := new(db.UnverifiedUser)
+	user.Username = request.Username
+	user.Email = request.Email
+	hashedPassword, err := util.HashAndSaltPassword(request.Password)
+
+	if err != nil {
+		return new(db.UnverifiedUser), err
+	}
+
+	user.Password = hashedPassword
+
+	generatedString, err := util.GenerateRandomString(64)
+
+	if err != nil {
+		return new(db.UnverifiedUser), err
+	}
+
+	user.VerificationCode = generatedString
+
+	return user, nil
+}
+
+func formatVerificationURL(verificationCode string, userID string) string {
+	if os.Getenv("PRODUCTION") == "" {
+		return fmt.Sprintf("http://localhost:3000/verify?v=%s&u=%s", verificationCode, userID)
+	} else {
+		return fmt.Sprintf("https://auth.quicky.dev/verify?v=%s&u=%s", verificationCode, userID)
+	}
+}
+
 // Register a new user
 func Register(c echo.Context) error {
-	credentials := new(RegisterCredentials)
+	credentials := new(RegisterRequest)
 
 	if err := c.Bind(credentials); err != nil {
 		return c.JSON(400, AuthError{"The body doesn't match RegisterCredentials"})
 	}
 
-	generatedString, err := util.GenerateRandomString(64)
+	user, err := registerRequestToUnverifiedUser(credentials)
 
 	if err != nil {
-		return c.JSON(500, AuthError{"Couldn't generate a secure string."})
+		log.Println(err.Error())
+		return c.JSON(500, AuthError{err.Error()})
 	}
 
-	return c.String(http.StatusOK, generatedString)
+	objectID, err := db.AddUnverifiedUser(user)
+
+	if err != nil {
+		log.Println(err.Error())
+		return c.JSON(500, AuthError{err.Error()})
+	}
+
+	return c.String(http.StatusOK, formatVerificationURL(user.VerificationCode, objectID))
 }
 
 // Login a pre existing user
