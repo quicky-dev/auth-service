06951f166599f15eac0d1e58b878b5049e94d8e9
C3NZ <cenz@cenz.io>
2020-03-19 11:31:12 -0700

    [fix] the routers so that they're now updated.

--- routers/auth.go
+++ routers/auth.go
@@ -2,10 +2,10 @@ package routers
 
 import (
 	"github.com/labstack/echo"
-	"github.com/quicky-dev/auth-service/controllers"
+	"github.com/quicky-dev/auth-service/controllers/auth"
 )
 
 // AttachAuth attaches all auth endpoints to the main echo instance
 func RegisterAuthRoutes(app *echo.Echo) {
-	app.POST("/auth/register", controllers.Register)
+	app.POST("/auth/register", auth.Register)
 }
