128b261bd7c63793b65ec6977566d3261bd5f991
C3NZ <cenz@cenz.io>
2020-03-22 16:21:08 -0700

    [add] an index for usernames making them unique, move all of the preparation so that it exists inside of the db class and anything password related is still done before saving, and add the mongodb error handling util.

--- db/user.go
+++ db/user.go
@@ -7,41 +7,95 @@ import (
 	"github.com/quicky-dev/auth-service/util"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"time"
 )
 
 var users = util.MongoClient.Database("Auth").Collection("Users")
 
+// TODO file an issue on the mongo-driver org, because this apparently creates
+// an index for usernames :thonk:
+func setupIndexes() error {
+	_, err := users.Indexes().CreateOne(
+		context.TODO(),
+		mongo.IndexModel{
+			Keys:    bson.M{"username": ""},
+			Options: options.Index().SetUnique(true)})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func initializeModel() bool {
+	err := setupIndexes()
+	log.Println(err)
+	return true
+}
+
+var Initialized = initializeModel()
+
 type User struct {
-	ID                         primitive.ObjectID `bson: "_id, omitempty"`
-	Username                   string             `bson:"thisname"`
+	ID                         primitive.ObjectID `bson:"_id,omitempty"`
+	Username                   string             `bson:"username"`
 	Email                      string             `bson:"email"`
 	Password                   string             `bson:"password"`
 	Verified                   bool               `bson:"verified"`
 	VerificationCode           string             `bson:"verificationCode"`
+	RefreshToken               string             `bson:"refreshToken"`
 	VerificationExpirationDate time.Time          `bson:"verificationExpirationDate"`
 	LastSignIn                 time.Time          `bson:"lastSignIn"`
 	CreatedAt                  time.Time          `bson:"createdAt"`
 }
 
+func prepareUserForInsertion(user *User) error {
+	hashedPassword, err := util.HashAndSaltPassword(user.Password)
+
+	if err != nil {
+		return err
+	}
+
+	user.Password = hashedPassword
+
+	generatedString, err := util.GenerateRandomString(64)
+	if err != nil {
+		return err
+	}
+
+	user.VerificationCode = generatedString
+	user.VerificationExpirationDate = time.Now().AddDate(0, 0, 2)
+	user.Verified = false
+
+	return nil
+}
+
 func (this *User) Save() (string, error) {
+	if err := prepareUserForInsertion(this); err != nil {
+		return "", err
+	}
+
 	insertion, err := users.InsertOne(context.TODO(), this)
 
 	if err != nil {
-		log.Printf(err.Error())
-		log.Printf("Couldn't create the verified this: %s", this.Username)
+		log.Println(err)
+		err = util.ConvertMongoErrorToAPIError(err)
+		return "", err
 	}
 
 	thisID := util.GetObjectIdFromInsertion(insertion.InsertedID)
 
 	if thisID == "" {
-		return "", fmt.Errorf("Couldn't obtain the ObjectID for this: %s", this.Username)
+		return "", fmt.Errorf("Couldn't obtain the ObjectID for user: %s", this.Username)
 	}
 
 	return thisID, nil
 }
 
+// Verify the email for a user given the
 func (this *User) VerifyEmail(objectID string, verificationCode string) (bool, error) {
 	objectIDHex, err := util.GetObjectIDFromString(objectID)
 
@@ -49,10 +103,15 @@ func (this *User) VerifyEmail(objectID string, verificationCode string) (bool, e
 		return false, err
 	}
 
-	filter := bson.M{"_id": objectIDHex, "verificationCode": verificationCode,
-		"verified": false, "verificationExpirationDate": bson.D{{"$gt", time.Now()}}}
-	update := bson.D{{"$set", bson.M{"verified": true, "verificationExpirationDate": time.Time{}}}}
-	log.Println(filter)
+	filter := bson.M{
+		"_id":                        objectIDHex,
+		"verificationCode":           verificationCode,
+		"verified":                   false,
+		"verificationExpirationDate": bson.D{{"$gt", time.Now()}}}
+
+	update := bson.D{{
+		"$set",
+		bson.M{"verified": true, "verificationExpirationDate": time.Time{}}}}
 
 	err = users.FindOneAndUpdate(context.TODO(), filter, update).Decode(&this)
 
@@ -63,21 +122,46 @@ func (this *User) VerifyEmail(objectID string, verificationCode string) (bool, e
 	return true, nil
 }
 
+// Login the user by verifying that the user exists and that the hashed & salted
+// password matches the hashed and salted password sent from the login request.
 func (this *User) Login() error {
 	filter := bson.M{"username": this.Username}
 	foundUser := new(User)
 
-	err := users.FindOne(context.TODO(), filter).Decode(&foundUser)
+	if err := users.FindOne(context.TODO(), filter).Decode(&foundUser); err != nil {
+		err = util.ConvertMongoErrorToAPIError(err)
+		return err
+	}
+
+	if foundUser.Verified == false {
+		return errors.New("The user is currently not verified.")
+	}
+
+	if success := util.ComparePasswordWithHash(this.Password, foundUser.Password); success != true {
+		return errors.New("Could not successfully login.")
+	}
+
+	generatedString, err := util.GenerateRandomString(48)
+
+	if err != nil {
+		return errors.New("Couldn't issue a refresh token.")
+	}
+
+	updateRefreshToken := bson.D{{"$set", bson.M{"refreshToken": generatedString, "lastSignIn": time.Now()}}}
+
+	err = users.FindOneAndUpdate(
+		context.TODO(), filter, updateRefreshToken).Decode(&foundUser)
+
 	if err != nil {
 		return err
 	}
 
-	success := util.ComparePasswordWithHash(this.Password, foundUser.Password)
+	this = foundUser
+	return nil
+}
 
-	if success {
-		this = foundUser
-		return nil
-	}
-
-	return errors.New("Could not successfully login.")
+// Get a new refresh token for a user given that the current refresh token
+// being used is valid.
+func (this *User) GetNewRefreshToken(currentRefreshToken string) error {
+	return nil
 }
